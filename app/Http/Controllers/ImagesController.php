0d34ce699242e995c651d7b02d316328e9f35b2c
Charlie Way <charlie.w@teemill.com>
2020-11-24 11:11:00 +0000

    Image paths are now retrieved from filesystem

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -3,40 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+
+//constant for number of images to retrieve
+define("NUM_OF_IMAGES", 4);
 
 class ImagesController extends Controller
 {
-    /* TODO: retrieve image paths from storage instead of hard coded
-    */
-    private $images = [
-        "/storage/images/butterfly.jpg",
-        "/storage/images/cat.jpg",
-        "/storage/images/dog.jpg",
-        "/storage/images/goldfish.jpg"
-    ];
-
     //ascending order
     public function asc() {
-        $ascendingImages = $this->images;
-        sort($ascendingImages);
+        $ascending_images = $this->retrieve_image_urls(NUM_OF_IMAGES);
+        sort($ascending_images);
 
-        return $ascendingImages;
+        return $ascending_images;
     }
 
     //descending order
     public function desc() {
-        $descendingImages = $this->images;
-        rsort($descendingImages);
+        $descending_images = $this->retrieve_image_urls(NUM_OF_IMAGES);
+        rsort($descending_images);
 
-        return $descendingImages;
+        return $descending_images;
     }
 
     //random order
     public function random() {
-        $randomImages = $this->images;
-        shuffle($randomImages);
+        $random_images = $this->retrieve_image_urls(NUM_OF_IMAGES);
+        shuffle($random_images);
 
-        return $randomImages;
+        Log::info($random_images);
+        return $random_images;
+    }
+
+    /* for each image path, check for Zone Identifier ADS, then remove /public/ and prepend /storage/
+    *  to get the public URL, then return $num_of_images images from the final array
+    */
+    private function retrieve_image_urls($num_of_images) {
+        $image_paths = Storage::files("public/images");
+        $urls = [];
+
+        foreach ($image_paths as $image) {
+            $adsSplit= explode(":", $image);
+            if (count($adsSplit) == 1) {
+                $pathSplit = explode("/", $image);
+                $pathSplit[0] = "storage";
+                $url = implode("/", $pathSplit);
+                array_push($urls, $url);
+            }
+        }
+
+        Log::info($urls);
+        return array_slice($urls, 0, $num_of_images);
     }
 
 }
