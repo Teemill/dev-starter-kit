8988b9f5140ac44932405ecf3d12659018032233
Charlie Way <charlie.w@teemill.com>
2020-11-24 15:22:16 +0000

    Migrations created for cars/fuels/manufacturers

---
+++ database/migrations/2020_11_24_122045_create_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger("fuel_type_id");
+            $table->unsignedBigInteger("manufacturer_id");
+            $table->string("name");
+            $table->tinyInteger("seats");
+            $table->tinyInteger("doors");
+            $table->smallInteger("top_speed");
+            $table->timestamps();          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
