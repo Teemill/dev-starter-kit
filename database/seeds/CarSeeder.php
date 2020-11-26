f30b3e7dedf50d23fca49f8a9c93be9756718ceb
Charlie Way <charlie.w@teemill.com>
2020-11-26 13:27:53 +0000

    Improved data seeding

--- database/seeds/CarSeeder.php
+++ database/seeds/CarSeeder.php
@@ -15,24 +15,17 @@ class CarSeeder extends Seeder
      */
     public function run()
     {
-        $faker = new Faker;
+        $fuelNames = ["Petrol", "Diesel", "Electric"];
+        $fuels = [];
 
-        $fuelOne = FuelType::create([
-            "name" => "Petrol"
-        ]);
-        $fuelOne->save();
-
-        $fuelTwo = FuelType::create([
-            "name" => "Diesel"
-        ]);
-        $fuelTwo->save();
-
-        $fuelThree = FuelType::create([
-            "name" => "Diesel"
-        ]);
-        $fuelThree->save();
-
-        $fuels = [$fuelOne, $fuelTwo, $fuelThree];
+        foreach ($fuelNames as $fuelName) 
+        {
+            $newFuel = FuelType::create([
+                "name" => $fuelName
+            ]);
+            $newFuel->save();
+            $fuels[] = $newFuel;
+        }
 
         $manufacturers = [
             "Ford",
@@ -42,27 +35,40 @@ class CarSeeder extends Seeder
             "Renault"
         ];
 
-        for ($i = 0; $i < 5; $i++) {
+        $carNames = [
+            "Fiesta",
+            "Mustang",
+            "GT-R",
+            "Micra",
+            "Model X",
+            "Model S",
+            "Golf",
+            "Polo",
+            "Clio",
+            "Megane"
+        ];
+
+        $carNameIndex = 0;
+        for ($i = 0; $i < 5; $i++) 
+        {
             $newManufacturer = Manufacturer::create([
                 "name" => $manufacturers[$i]
             ]);
             $newManufacturer->save();
 
-            for ($j = 0; $j < 2; $j++) {
+            for ($j = 0; $j < 2; $j++) 
+            {
                 $fuel = $fuels[$i % 3];
                 $newCar = Car::create([
                     "fuel_type_id" => $fuel->id,
                     "manufacturer_id" => $newManufacturer->id,
-                    "name" => "Car Name " . $i . $j,
+                    "name" => $carNames[$carNameIndex],
                     "seats" => 4,
                     "doors" => 4,
                     "top_speed" => 200
                 ]);
                 $newCar->save();
-                /*
-                $newCar->fuelType()->save($fuel);
-                $newCar->manufacturer()->save($newManufacturer);
-                */
+                $carNameIndex++;
             }
         }
     }
