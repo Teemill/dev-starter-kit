8d90206c68d6ec2817e2e79dc6d6ba4d480ffb87
Charlie Way <charlie.w@teemill.com>
2020-11-26 13:24:37 +0000

    Added middlewear to check paassword

---
+++ app/Http/Middleware/CheckPassword.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckPassword
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->password != env("NEW_CAR_PASSWORD")) {
+            return response("Not allowed", 403);
+        }
+
+        return $next($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,17 +23,20 @@ Route::get('api-test', function () {
 
 // image api routes
 Route::get('images/asc', "ImageController@asc");
-
 Route::get('images/desc', "ImageController@desc");
-
 Route::get('images/random', "ImageController@random");
 
 // animal api routes
 Route::get('animals/list', "AnimalController@alphabetical");
-
 Route::get('animals/get/{animal_id}', "AnimalController@retrieve");
 
 // car api routes
 Route::get('cars/list', "CarController@index");
-
+Route::get("cars/count/{property}", "CarController@countOn");
 Route::get('cars/get/{id}', "CarController@retrieve");
+Route::post("cars/add", "CarController@store")->middleware("check_password");
+
+Route::get('manufacturers/list', "ManufacturerController@index");
+Route::get("manufacturers/data", "ManufacturerController@data");
+Route::get('fuel_types/list', "FuelTypeController@index");
+Route::get("fuel_types/data", "FuelTypeController@data");
