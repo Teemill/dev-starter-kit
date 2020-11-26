3d5536bfa90e17fb0788673933af5aff8a524eeb
Charlie Way <charlie.w@teemill.com>
2020-11-26 13:30:35 +0000

    added navigation component routed from '/'

---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="navigation">
+        <table>
+            <router-link to="/animals/list" tag="tr" class="selectable"><th>Animals</th></router-link>
+            <router-link to="/cars/list" tag="tr" class="selectable"><th>Cars</th></router-link>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
